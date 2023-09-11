cb8aded3a43a9049aad27de126a6765ab1fccd39
an0706sy <99664732+an0706sy@users.noreply.github.com>
2023-09-11 16:37:25 +0900

    Add files via upload

---
+++ ReadTxtFile/ReadTxtFileDlg.h
@@ -0,0 +1,49 @@
+﻿
+// ReadTxtFileDlg.h: 헤더 파일
+//
+
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+struct TxtData
+{
+	CString Line;
+};
+// CReadTxtFileDlg 대화 상자
+class CReadTxtFileDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	TxtData m_LineData[500];
+	vector<TxtData> m_Data;
+	CReadTxtFileDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_READTXTFILE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	void OpenFile(CString FilePath);
+	void ShowData();
+	afx_msg void OnBnClickedOpen();
+	
+	CListCtrl m_ListCtrl;
+};
